a09360b40575dafbaf2e16ddf72e8786b1ca28b7
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-07-10 20:41:38 +0700

    t

--- src/components/TelegramWebApp.tsx
+++ src/components/TelegramWebApp.tsx
@@ -5,21 +5,8 @@ import { useEffect } from 'react';
 const TelegramWebApp = () => {
   useEffect(() => {
     if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
-      const initData = window.Telegram.WebApp.initData;
-      const initDataUnsafe = window.Telegram.WebApp.initDataUnsafe;
-      
-      console.log('Init data:', initData);
-      console.log('Init data unsafe:', initDataUnsafe);
-
-      window.Telegram.WebApp.ready();
-    } else {
-      console.error('Telegram WebApp is not defined');
-    }
-  }, []);
-
-  useEffect(() => {
-    if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
       const tg = window.Telegram.WebApp;
+
       tg.expand(); // Ensure the web app is expanded to cover the full screen
 
       tg.onEvent('themeChanged', setTheme); // React to theme changes
@@ -37,8 +24,8 @@ const TelegramWebApp = () => {
       const tg = window.Telegram.WebApp;
       const theme = tg.themeParams;
 
-      // Set your background color according to the theme
-      document.body.style.backgroundColor = "#222325";
+      // Set background color according to the theme
+      tg.setBackground("#222325");
     }
   };
 
