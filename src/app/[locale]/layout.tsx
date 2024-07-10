d1d31eb44b7fed60eee4ad46778c6f3c7df5760a
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-07-10 16:49:36 +0700

    tg

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script async src="https://telegram.org/js/telegram-web-app.js"></script>
+      </head>
       <body>
         <Providers>
           {children}
