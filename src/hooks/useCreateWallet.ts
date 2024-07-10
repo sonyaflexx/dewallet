685c604032b9d62cddd84bd61568813b538e1671
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-07-10 19:20:14 +0700

    test

--- src/hooks/useCreateWallet.ts
+++ src/hooks/useCreateWallet.ts
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import axios from 'axios';
 import WalletStore from '@/store/WalletStore';
 import { mnemonicNew, mnemonicToWalletKey } from '@ton/crypto';
@@ -6,40 +6,39 @@ import { mnemonicNew, mnemonicToWalletKey } from '@ton/crypto';
 export const useCreateWallet = () => {
   const [wallet, setWallet] = useState<any>(null);
 
-  useEffect(() => {
-    const generateWallet = async () => {
-      try {
-        const mnemonicArrayResult = await mnemonicNew(24);
-        const keyPairResult = await mnemonicToWalletKey(mnemonicArrayResult);
+  const generateWallet = useCallback(async () => {
+    try {
+      const mnemonicArrayResult = await mnemonicNew(24);
+      const keyPairResult = await mnemonicToWalletKey(mnemonicArrayResult);
 
-        const newWallet = {
-          mnemonic: {
-            phrase: mnemonicArrayResult.join(' ')
-          },
-          address: Array.prototype.map.call(keyPairResult.publicKey, x => ('00' + x.toString(16)).slice(-2)).join(''),
-          privateKey: Array.prototype.map.call(keyPairResult.secretKey, x => ('00' + x.toString(16)).slice(-2)).join('')
-        };
+      const newWallet = {
+        mnemonic: {
+          phrase: mnemonicArrayResult.join(' ')
+        },
+        address: Array.prototype.map.call(keyPairResult.publicKey, x => ('00' + x.toString(16)).slice(-2)).join(''),
+        privateKey: Array.prototype.map.call(keyPairResult.secretKey, x => ('00' + x.toString(16)).slice(-2)).join('')
+      };
 
-        setWallet(newWallet);
-      } catch (error) {
-        console.error('Failed to generate wallet:', error);
-      }
-    };
-
-    generateWallet();
+      setWallet(newWallet);
+      WalletStore.setWallet(newWallet);
+    } catch (error) {
+      console.error('Failed to generate wallet:', error);
+    }
   }, []);
 
-  const createWallet = async () => {
+  useEffect(() => {
+    generateWallet();
+  }, [generateWallet]);
+
+  const createWallet = useCallback(async () => {
     if (wallet) {
-      WalletStore.setWallet(wallet);
-      
       try {
         await axios.post('/api/wallet/create', wallet);
       } catch (error) {
         console.error('Failed to save wallet to the database:', error);
       }
     }
-  };
+  }, [wallet]);
 
-  return { createWallet, wallet };
+  return { createWallet, wallet, generateWallet };
 };
