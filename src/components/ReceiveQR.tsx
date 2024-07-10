9f42ff001e3476caeb017ffdcc9aa2975ff53400
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-07-10 16:31:30 +0700

    f

--- src/components/ReceiveQR.tsx
+++ src/components/ReceiveQR.tsx
@@ -1,45 +1,45 @@
-import QRCode from 'qrcode.react';
-import logoImage from './logo.png'; // Подставьте путь к вашей картинке
+// import QRCode from 'qrcode.react';
+// import logoImage from './logo.png'; // Подставьте путь к вашей картинке
 
-const RoundedQRCode = () => {
-  const qrConfig = {
-    value: 'https://example.com',
-    size: 256,
-    bgColor: '#FFFFFF',
-    fgColor: '#000000', 
-    level: 'H',
-    renderAs: "svg",
-  };
+// const RoundedQRCode = () => {
+//   const qrConfig = {
+//     value: 'https://example.com',
+//     size: 256,
+//     bgColor: '#FFFFFF',
+//     fgColor: '#000000', 
+//     level: 'H',
+//     renderAs: "svg",
+//   };
 
-  const qrContainerStyles = {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flexDirection: 'column',
-    background: qrConfig.bgColor,
-    padding: '20px',
-    borderRadius: '20px',
-    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
-  };
+//   const qrContainerStyles = {
+//     display: 'flex',
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     flexDirection: 'column',
+//     background: qrConfig.bgColor,
+//     padding: '20px',
+//     borderRadius: '20px',
+//     boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
+//   };
 
-  return (
-    <div style={{ qrContainerStyles }}>
-      <QRCode
-        value={qrConfig.value}
-        size={qrConfig.size}
-        bgColor={qrConfig.bgColor}
-        fgColor={qrConfig.fgColor}
-        level={qrConfig.level}
-        renderAs={qrConfig.renderAs}
-        logoImage={logoImage} // Картинка для вставки в центр QR-кода
-        logoWidth={64} // Ширина картинки
-        logoHeight={64} // Высота картинки
-      />
-      <p style={{ marginTop: '10px', fontSize: '18px', color: '#333' }}>
-        Scan the QR code
-      </p>
-    </div>
-  );
-};
+//   return (
+//     <div style={{ qrContainerStyles }}>
+//       <QRCode
+//         value={qrConfig.value}
+//         size={qrConfig.size}
+//         bgColor={qrConfig.bgColor}
+//         fgColor={qrConfig.fgColor}
+//         level={qrConfig.level}
+//         renderAs={qrConfig.renderAs}
+//         logoImage={logoImage} // Картинка для вставки в центр QR-кода
+//         logoWidth={64} // Ширина картинки
+//         logoHeight={64} // Высота картинки
+//       />
+//       <p style={{ marginTop: '10px', fontSize: '18px', color: '#333' }}>
+//         Scan the QR code
+//       </p>
+//     </div>
+//   );
+// };
 
-export default RoundedQRCode;
+// export default RoundedQRCode;
