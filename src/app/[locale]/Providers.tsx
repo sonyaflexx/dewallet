4d5bb0f242ea17a8b3dd99d9a4e59d74d1e030be
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-07-15 21:01:31 +0700

    test

--- src/app/[locale]/Providers.tsx
+++ src/app/[locale]/Providers.tsx
@@ -35,7 +35,7 @@ export default function Providers({ children }: { children: ReactNode }) {
   useEffect(() => {
     if (typeof window !== 'undefined' && window.Telegram && window.Telegram.WebApp) {
       const webApp = window.Telegram.WebApp;
-      // webApp.colorScheme
+      webApp.themeParams.button_color='#222328'
       webApp.setHeaderColor('#222328');
       webApp.setBackgroundColor('#222328')
 
