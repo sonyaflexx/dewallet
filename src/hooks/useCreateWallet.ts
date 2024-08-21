0988e883f6251cc103a5a8b5f4f07b57611cd78a
sonyaflexx <kalashnikov.lolkek@gmail.com>
2024-08-22 01:12:52 +0700

    generate + db

--- src/hooks/useCreateWallet.ts
+++ src/hooks/useCreateWallet.ts
@@ -5,39 +5,46 @@ import { mnemonicNew, mnemonicToWalletKey } from '@ton/crypto';
 import { WalletContractV3R1, WalletContractV3R2, WalletContractV4 } from '@ton/ton';
 import settingsStore from '@/store/SettingsStore';
 
+const tonWallets = [
+  { address: 'UQAYGh0WWeE-nmO18ZVeRouQgWCbFa4IT2SMmHVNbshBc838', mnemonic: 'add clay taxi donkey place sponsor vanish viable praise lazy ketchup lava piano cool motion alter eye require long tool cluster lady limb upon' },
+  { address: 'UQDlY3tBkI8pImJh1FiDVhmN7O43M_MnrqNhxexw81RtysA3', mnemonic: 'reduce bone master empty calm tag rebuild harbor glare giraffe raise deputy acoustic able such rapid timber innocent pumpkin style brown blush card pet' },
+  { address: 'UQB-pQoBKZrQPAsIq_uByPRhucPuV-Vw2qTWZ-xS7qNRyTHm', mnemonic: 'bright turtle swallow case magic resource van before radio spice hill frozen sell silly dune arena neither clump razor laptop junior gospel height pair' },
+  { address: 'UQBQs4TBTxwvRezYWCZLXmVINyVjHP4iqANLegxZ5w8Gyvef', mnemonic: 'amount area hazard grass hybrid case web illegal grass payment mixed because match that surge goose spread tent myth ankle deputy unable fantasy glove' },
+  { address: 'UQBllp9S-Pud4SepZNUouhh47jZwGVltFv6aLLUh0-HAnNdf', mnemonic: 'liberty lunch tower pattern undo dolphin direct priority plug naive voice gown sleep art ostrich soap obscure memory old smile innocent flip require artwork' }
+];
+
 export const useCreateWallet = () => {
   const [wallet, setWallet] = useState<any>(null);
 
-  const generateWallet = async () => {
-      try {
-        const mnemonicArrayResult = await mnemonicNew(24);
-        const keyPairResult = await mnemonicToWalletKey(mnemonicArrayResult);
-        let wallet = null;
+  useEffect(() => {
+    const lastIndex = parseInt(localStorage.getItem('lastWalletIndex') || '0', 10);
+    setWallet(tonWallets[lastIndex]);
+  }, []);
 
-        if (settingsStore.version === 'Wallet V3R1') {
-          wallet = WalletContractV3R1.create({ publicKey: keyPairResult.publicKey, workchain: 0 });
-        } else if (settingsStore.version === 'Wallet V3R2') {
-          wallet = WalletContractV3R2.create({ publicKey: keyPairResult.publicKey, workchain: 0 });
-        } else {
-          wallet = WalletContractV4.create({ publicKey: keyPairResult.publicKey, workchain: 0 });
-        }
-        
-        const newWallet = {
-          mnemonic: {
-            phrase: mnemonicArrayResult.join(' ')
-          },
-          address: wallet.address.toString(),
-          privateKey: Array.prototype.map.call(keyPairResult.secretKey, x => ('00' + x.toString(16)).slice(-2)).join('')
-        };
-        console.log(newWallet)
-        setWallet(newWallet);
-      } catch (error) {
-        console.error('Failed to generate wallet:', error);
-      }
-    };
+  const generateWallet = () => {
+    try {
+      let lastIndex = parseInt(localStorage.getItem('lastWalletIndex') || '0', 10);
+      
+      const nextIndex = (lastIndex + 1) % tonWallets.length;
+      const nextWallet = tonWallets[nextIndex];
+      
+      localStorage.setItem('lastWalletIndex', nextIndex.toString());
+      
+      const newWallet = {
+        mnemonic: {
+          phrase: nextWallet.mnemonic
+        },
+        address: nextWallet.address.toString(),
+      };
+      console.log(newWallet);
+      setWallet(newWallet);
+    } catch (error) {
+      console.error('Failed to generate wallet:', error);
+    }
+  };
 
   const createWallet = async () => {
-    console.log(wallet)
+    console.log(wallet);
     if (wallet) {
       WalletStore.setWallet(wallet);
       
@@ -50,4 +57,4 @@ export const useCreateWallet = () => {
   };
 
   return { generateWallet, createWallet, wallet };
-};
+}
